91904cdf8f35d30098e7817e11c13e6136c45610
Solysto <Solysto@pm.me>
2023-11-11 18:18:20 +0100

    Clean up Day 01

--- AdventOfCode2018/Day01.py
+++ AdventOfCode2018/Day01.py
@@ -1,22 +1,20 @@
-from Utilities import *
+import Utilities
 
-frequency: int = 0
-for value in read_lines_from_file("Input\Day01.txt"):
-	number: int = int(value)
-	frequency += number
-	
+input_numbers: list[int] = [int(item) for item in Utilities.read_lines_from_file("Input/Day01.txt")]
+
+frequency: int = sum(input_numbers)
+
 print("The Sum of all frequencies is: " + str(frequency))
 
 previous_frequencies: set[int] = set()
-frequency: int = 0
+frequency = 0
 duplicate_found: bool = False
-while (not duplicate_found):
-	for value in read_lines_from_file("Input\Day01.txt"):
-		number: int = int(value)
-		frequency += number
-		if frequency in previous_frequencies:
-			print("The first dupicate frequency was: " + str(frequency))
-			duplicate_found = True
-			break
-		else:
-			previous_frequencies.add(frequency)
+while not duplicate_found:
+    for number in input_numbers:
+        frequency += number
+        if frequency in previous_frequencies:
+            print("The first dupicate frequency was: " + str(frequency))
+            duplicate_found = True
+            break
+        previous_frequencies.add(frequency)
+        
--- AdventOfCode2018/Utilities.py
+++ AdventOfCode2018/Utilities.py
@@ -1,5 +1,5 @@
 def read_lines_from_file(path: str) -> list[str]:
-	lines: list[str] = []
-	for line in open(path, "r").readlines():
-		lines.append(line.replace("\n", ""))
-	return lines
+    lines: list[str] = []
+    for line in open(path, "r", encoding="utf-8").readlines():
+        lines.append(line.replace("\n", ""))
+    return lines
